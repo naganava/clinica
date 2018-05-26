6e1760a554aae349a9184e36cc51df045019cba1
Rafael <rflg@outlook.com>
2018-05-26 17:14:38 -0300

    Criação de Relatórios.

---
+++ src/relatorios/Receita.java
@@ -0,0 +1,18 @@
+package relatorios;
+
+import pessoas.Paciente;
+
+/**
+ *
+ * @author Rafael
+ */
+public class Receita extends RelatorioMedico{
+       
+    private Paciente paciente;
+    private String receita;
+    
+    public void gerarRelatorio(){
+        
+    }
+    
+}
---
+++ src/relatorios/RelatorioMedico.java
@@ -0,0 +1,13 @@
+package relatorios;
+
+import pessoas.Medico;
+
+/**
+ *
+ * @author Rafael
+ */
+public class RelatorioMedico extends Relatorio{
+
+    private Medico medico;
+    
+}
